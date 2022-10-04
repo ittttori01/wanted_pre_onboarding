81925e609566b6c59a0b9745cf340a6f85a96d42
ittttori01 <ittttori01@gmail.com>
2022-10-04 09:12:57 +0900

    first

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const sequelize = require('./database');
+const Company = require('./Company');
+
+sequelize.sync({force:true}).then(()=>{
+    
+})
+const app = express();
+app.use(express.json());
+// app.post('/company',(req,res)=>{
+
+//     Company.create(req.body
+//     ).then(()=>{
+      
+//         res.send('success')
+//     })
+//     .catch((err)=>{
+//         console.log(err)
+//     })
+
+// });
+
+// app.get('/company',async(req,res)=>{
+
+//    const company = await Company.findAll();
+//    res.send(company)
+// })
+
+const router = require('./routes')(app);
+
+
+console.log(Company)
+app.listen(3000,()=>{
+
+    console.log("APP IS RUNNING");
+});
