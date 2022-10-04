11194c1938eb3cd0e962fcccecb5116feba1dcfc
ittttori01 <ittttori01@gmail.com>
2022-10-04 09:48:31 +0900

    edit

--- controllers/company/company.js
+++ controllers/company/company.js
@@ -1,10 +1,26 @@
 const Company = require('../../Company');
+const _uuid = require('uuid');
+const _crypto = require('crypto');
 
-
-exports.register = async(req,res) => {
+exports.register = (req,res) => {
     
-    await Company.create(req.body);
-    res.send('successed');
+    let uuid = _uuid.v4();
+
+    Company.create({
+
+        uuid : uuid,
+        country : req.body.country,
+        region : req.body.region
+    })
+    .then(()=>{
+        
+        res.status(200).json({
+            uuid: uuid,
+            message : "Compayny Has Been Registered"
+        
+        });
+
+    });
 
 };
 
--- controllers/job_posting/Job_Posting.js
+++ controllers/job_posting/Job_Posting.js
@@ -4,7 +4,13 @@ exports.register = async(req,res) => {
 
     await JobPosting.create(req.body);
 
-    console.log()
     res.send('Content has been Posted');
+    
+};
+
+exports.edit = (req,res) => {
+
+    let company_uuid = req.body.uuid;
+    let post_id = req.body.post_id;
     
 }
