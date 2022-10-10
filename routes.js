7fde2c44e2f185f72538e9174760437e9c01bff3
ittttori01 <ittttori01@gmail.com>
2022-10-11 08:54:53 +0900

    url 변경

--- routes.js
+++ routes.js
@@ -1,16 +1,16 @@
 const Router = (app) => {
 
-    const Company = require('./controllers/company/company');
-    app.post("/company", (req, res) =>Company.register(req,res));
-    app.get("/company",(req,res) => Company.getAllCompany(req,res));
+  const Company = require('./controllers/company/company');
+  app.post("/company", (req, res) =>Company.register(req,res));
+  app.get("/company",(req,res) => Company.getAllCompany(req,res));
 
-    const JobPosting = require('./controllers/job_posting/Job_Posting');
-    app.get("/list",(req,res) => JobPosting.list(req,res));
-    app.get("/list/:posting_id",(req,res) => JobPosting.listDetail(req,res));
-    app.post("/list",(req,res) => JobPosting.register(req,res));
-    app.put("/list/edit",(req,res)=> JobPosting.edit(req,res));
-    app.delete("/list",(req,res) => JobPosting.remove(req,res));
+  const JobPosting = require('./controllers/job_posting/Job_Posting');
+  app.get("/list",(req,res) => JobPosting.list(req,res));
+  app.get("/list/:posting_id",(req,res) => JobPosting.listDetail(req,res));
+  app.post("/list",(req,res) => JobPosting.register(req,res));
+  app.put("/list/edit",(req,res)=> JobPosting.edit(req,res));
+  app.delete("/list",(req,res) => JobPosting.remove(req,res));
 
 };
-  
-  module.exports = Router;
+
+module.exports = Router;
