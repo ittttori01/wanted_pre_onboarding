c6270ed2b39c8c49956382a13b12548b8847a8c0
ittttori01 <ittttori01@gmail.com>
2022-10-12 09:58:22 +0900

    delete code

--- server.js
+++ server.js
@@ -2,32 +2,15 @@ const express = require('express');
 const sequelize = require('./database');
 
 sequelize.sync({force:true}).then(()=>{
-    
+    console.log("DATABSE IS RUUNING");
 })
 const app = express();
 app.use(express.json());
-// app.post('/company',(req,res)=>{
-
-//     Company.create(req.body
-//     ).then(()=>{
-      
-//         res.send('success')
-//     })
-//     .catch((err)=>{
-//         console.log(err)
-//     })
-
-// });
-
-// app.get('/company',async(req,res)=>{
-
-//    const company = await Company.findAll();
-//    res.send(company)
-// })
 
 const router = require('./routes')(app);
 
 app.listen(3000,()=>{
 
     console.log("APP IS RUNNING");
+
 });
